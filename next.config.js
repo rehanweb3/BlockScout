ad5dc3fa54428e0f0998266078d1cf9a5b14c45f
Muhammad Rehan <rehanje1215@gmail.com>
2026-03-31 06:52:53 -0700

    Updated project structure: re-added server files and updated configs

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
